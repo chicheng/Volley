f2306b9e809eb04f39d638d9d0b31b8638495044
CHI Cheng <git@chicheng.me>
2014-11-20 00:24:06 +1100

    * use disable ssl v3 socket and remove support for android 2.2 and older

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -17,13 +17,11 @@
 package com.android.volley.toolbox;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.net.http.AndroidHttpClient;
 import android.os.Build;
 
 import com.android.volley.Network;
 import com.android.volley.RequestQueue;
+import com.android.volley.compat.DisableSSLv3SecureSocketFactory;
 
 import java.io.File;
 
@@ -42,22 +40,12 @@ public class Volley {
     public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
         File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
 
-        String userAgent = "volley/0";
-        try {
-            String packageName = context.getPackageName();
-            PackageInfo info = context.getPackageManager().getPackageInfo(packageName, 0);
-            userAgent = packageName + "/" + info.versionCode;
-        } catch (NameNotFoundException e) {
-        }
-
         if (stack == null) {
-            if (Build.VERSION.SDK_INT >= 9) {
-                stack = new HurlStack();
-            } else {
-                // Prior to Gingerbread, HttpUrlConnection was unreliable.
-                // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-                stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
-            }
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+				stack = new HurlStack(null, new DisableSSLv3SecureSocketFactory());
+			} else {
+				stack = new HurlStack();
+			}
         }
 
         Network network = new BasicNetwork(stack);
